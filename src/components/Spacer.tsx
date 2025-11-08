5a3ef6a5c6ec4d536d3044f3b33c41de1d9560d8
impucky <raphael.bgrd@gmail.com>
2025-11-08 22:06:15 +0100

    content + homepage

--- src/components/Spacer.tsx
+++ src/components/Spacer.tsx
@@ -1,6 +1,14 @@
-export default function Spacer({ label }: { label: string }) {
+export default function Spacer({ label = "" }: { label?: string }) {
+  if (label === "") {
+    return (
+      <div className="m-4 w-full px-4 opacity-80">
+        <div className="border-t border-[salmon]"></div>
+      </div>
+    );
+  }
+
   return (
-    <div className="mt-5 flex w-full items-center justify-center px-4 opacity-80">
+    <div className="mt-5 flex w-full items-center px-4 opacity-80">
       <div className="flex-1 border-t border-[salmon]"></div>
       <span className="mx-4 text-xl font-bold text-[salmon]">{label}</span>
       <div className="flex-1 border-t border-[salmon]"></div>
