a2e052a524a725e27a278b128f01cd49bfa1f917
impucky <raphael.bgrd@gmail.com>
2025-11-17 21:18:39 +0100

    SEO pass

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig({
-  integrations: [react(), icon()],
+import sitemap from "@astrojs/sitemap";
 
+export default defineConfig({
+  integrations: [react(), icon(), sitemap()],
+  site: "https://fuzzyfest.live",
   vite: {
     plugins: [tailwindcss(), svgr()],
   },
